d0cfb797396c9fd1dcf30c1519e26236e04af3fa
richn <rolik.alexx@gmail.com>
2023-09-24 00:30:01 +0300

    edit book repository

--- src/main/java/book/store/bookstore/repository/BookRepository.java
+++ src/main/java/book/store/bookstore/repository/BookRepository.java
@@ -1,10 +1,14 @@
 package book.store.bookstore.repository;
 
 import book.store.bookstore.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
 
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository {
+    Book save(Book book);
+
+    Optional<Book> findById(Long id);
+
+    List<Book> findAll();
 
 }
